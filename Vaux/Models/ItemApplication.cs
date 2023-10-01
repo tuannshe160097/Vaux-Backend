3724a019e41efd580170048cbd0eaf0c75a583ab
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-10-01 23:03:32 +0700

    add category to item application

--- Vaux/Models/ItemApplication.cs
+++ Vaux/Models/ItemApplication.cs
@@ -6,6 +6,10 @@ namespace Vaux.Models
     {
         public int Id { get; set; }
         public int Status { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string? StatusChangeReason { get; set; }
