981aaa3d93f75b183f4fb583ab40eb39ffd58579
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-10-07 21:08:53 +0700

    update db

---
+++ Vaux/Migrations/20231006165433_AddStatusSellerPayment.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vaux.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusSellerPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SellerPayments_SuperUsers_ApprovedById",
+                table: "SellerPayments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApprovedById",
+                table: "SellerPayments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SellerPayments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SellerPayments_SuperUsers_ApprovedById",
+                table: "SellerPayments",
+                column: "ApprovedById",
+                principalTable: "SuperUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SellerPayments_SuperUsers_ApprovedById",
+                table: "SellerPayments");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SellerPayments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApprovedById",
+                table: "SellerPayments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SellerPayments_SuperUsers_ApprovedById",
+                table: "SellerPayments",
+                column: "ApprovedById",
+                principalTable: "SuperUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Vaux/Models/SellerPayment.cs
+++ Vaux/Models/SellerPayment.cs
@@ -4,12 +4,13 @@
     {
         public int Id { get; set; }
         public long Amount { get; set; }
+        public int Status { get; set; }
 
         public int? OrderId { get; set; }
         public Order? Order { get; set; }
 
-        public int ApprovedById { get; set; }
-        public SuperUser ApprovedBy { get; set; }
+        public int? ApprovedById { get; set; }
+        public SuperUser? ApprovedBy { get; set; }
 
         public int? UserId { get; set; }
         public User? User { get; set; }
