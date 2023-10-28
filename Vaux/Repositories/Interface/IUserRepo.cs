bdf121a40aa92a035e7d23e0ee6c1d1c20f07cb4
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-10-28 14:58:25 +0700

    Update IUserRepo.cs

--- Vaux/Repositories/Interface/IUserRepo.cs
+++ Vaux/Repositories/Interface/IUserRepo.cs
@@ -6,7 +6,7 @@ namespace Vaux.Repositories.Interface
 {
     public interface IUserRepo : IBaseRepo<User>
     {
-        public ResultListDTO<TOut> Search<TOut>(string search, int? role, int skip = 0, int take = -1)
+        public ResultListDTO<TOut> Search<TOut>(string search, int? role, int skip = 0, int take = -1);
         public TOut Create<TOut>(UserStrictDTO user, RoleId role);
         public void VerifyAccount(int id);
         public void ChangeAccess(int id);
