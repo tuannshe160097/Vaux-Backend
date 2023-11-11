3b6045eef6c6dc1606d8e17111381eb5f213b29f
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-11-11 14:29:50 +0700

    I hate myself

--- Vaux/Controllers/SellerApplicationController.cs
+++ Vaux/Controllers/SellerApplicationController.cs
@@ -58,7 +58,7 @@ namespace Vaux.Controllers
         [HttpGet]
         [Authorize(Roles = $"{nameof(RoleId.MODERATOR)},{nameof(RoleId.ADMIN)}")]
         [Route("/api/Seller/Application/GetAll")]
-        public IActionResult GetAll(int id)
+        public IActionResult GetAll()
         {
             var s = _sellerApplicationRepo.GetAll<SellerApplicationOutDTO>(e => e.Status);
             return Ok(s);
