bcb940de315b674a7bdc1c2c630c130eb7d90c0a
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-11-16 21:47:48 +0700

    Bug Fixes

--- Vaux/Repositories/Interface/ISellerApplicationRepo.cs
+++ Vaux/Repositories/Interface/ISellerApplicationRepo.cs
@@ -1,9 +1,11 @@
-﻿using Vaux.DTO;
+﻿using System.Linq.Expressions;
+using Vaux.DTO;
 using Vaux.Models;
 
 namespace Vaux.Repositories.Interface
 {
     public interface ISellerApplicationRepo : IBaseRepo<SellerApplication>
     {
+        public TOut Update<TOut, TIn>(Expression<Func<SellerApplication, bool>> predicate, TIn data, string changeReason);
     }
 }
