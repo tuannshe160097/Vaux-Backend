04289d7087ecb16fb8bf65a106f95361f2f62f9a
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-11-20 21:37:02 +0700

    Removed dumb space

--- Vaux/Repositories/SmsRepo.cs
+++ Vaux/Repositories/SmsRepo.cs
@@ -53,7 +53,7 @@ namespace Vaux.Repositories
 
         public bool PhoneNumberCheck(string phone)
         {
-            return Regex.IsMatch(phone, "/(84|0[3|5|7|8|9])+([0-9]{ 8})\b/g");
+            return Regex.IsMatch(phone, "/(84|0[3|5|7|8|9])+([0-9]{8})\b/g");
         }
     }
 }
