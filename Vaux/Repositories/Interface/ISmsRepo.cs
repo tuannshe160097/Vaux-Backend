0dc847b18f3a94b1bb65ff6c723a3bbd20456699
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-11-21 21:44:51 +0700

    Resolved Conflict

---
+++ Vaux/Repositories/Interface/ISmsRepo.cs
@@ -0,0 +1,8 @@
+﻿namespace Vaux.Repositories.Interface
+{
+    public interface ISmsRepo
+    {
+        public void SendSms(string phone, string content);
+        public bool PhoneNumberCheck(string phone);
+    }
+}
