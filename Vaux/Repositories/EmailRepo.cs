7623e3a38046385c443e65da344d9d2a2c1cbb97
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-11-26 00:10:58 +0700

    Added Email Sending

---
+++ Vaux/Repositories/EmailRepo.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Vaux.Repositories.Interface;
+
+namespace Vaux.Repositories
+{
+    public class EmailRepo : IEmailRepo
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Vaux", "haipham02122002@gmail.com"));
+            message.To.Add(MailboxAddress.Parse(to));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+            SmtpClient smtpClient = new SmtpClient();
+
+            try
+            {
+                smtpClient.Connect("smtp.gmail.com", 465, true);
+                smtpClient.AuthenticationMechanisms.Remove("NTLM");
+                //smtpClient.Authenticate("haipham02122002@gmail.com", ""); //Ask me for password
+                smtpClient.Send(message);
+                Console.WriteLine("Email sent");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                smtpClient.Disconnect(true);
+                smtpClient.Dispose();
+            }
+        }
+    }
+}
