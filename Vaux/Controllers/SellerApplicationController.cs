7efce48c3f9a276ce4e82dcfb7c33c357aad26ce
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-12-10 04:02:32 +0700

    Update SellerApplicationController.cs

--- Vaux/Controllers/SellerApplicationController.cs
+++ Vaux/Controllers/SellerApplicationController.cs
@@ -38,7 +38,7 @@ namespace Vaux.Controllers
             {
                 return BadRequest("Đơn đăng ký đã tồn tại!");
             }
-            var sam = _userRepo.Get<User>(e => e.Email == sellerApplication.Email);
+            var sam = _userRepo.Get<User>(e => e.Email == sellerApplication.Email && e.Id.ToString() != User.Identity!.Name);
             if(sam != null)
             {
                 return BadRequest("Email đã được sử dụng!");
@@ -146,7 +146,7 @@ namespace Vaux.Controllers
             {
                 return BadRequest("Đơn đăng ký không tồn tại!");
             }
-            var sam = _userRepo.Get<User>(e => e.Email == i.Email);
+            var sam = _userRepo.Get<User>(e => e.Email == i.Email && e.Id.ToString() != User.Identity!.Name);
             if (sam != null)
             {
                 return BadRequest("Email đã được sử dụng!");
