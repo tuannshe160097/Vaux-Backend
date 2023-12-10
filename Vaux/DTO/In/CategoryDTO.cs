caf130a26999ff2cb8170acf2661a08435226360
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-12-10 23:55:32 +0700

    Update CategoryDTO.cs

--- Vaux/DTO/In/CategoryDTO.cs
+++ Vaux/DTO/In/CategoryDTO.cs
@@ -5,6 +5,7 @@ namespace Vaux.DTO.In
     public class CategoryDTO
     {
         [MaxLength(30)]
+        [Required]
         public string Name { get; set; }
         [MaxLength(256)]
         public string? Description { get; set; }
