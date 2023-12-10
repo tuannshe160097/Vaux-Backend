900423fb5509d6087b03e123839ef97084c70d6a
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-12-10 23:54:38 +0700

    category dto

---
+++ Vaux/DTO/Out/CategoryOutDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Vaux.DTO.Out
+{
+    public class CategoryOutDTO
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
