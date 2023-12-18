346ded973ed4287c0c69ee27a5e3ee5386650dbf
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2023-12-18 18:59:22 +0700

    vnpay bug fix

--- Vaux/Repositories/VnPayServiceRepo.cs
+++ Vaux/Repositories/VnPayServiceRepo.cs
@@ -22,7 +22,7 @@ namespace Vaux.Repositories
             pay.AddRequestData("vnp_Version", _configuration["Vnpay:Version"]);
             pay.AddRequestData("vnp_Command", _configuration["Vnpay:Command"]);
             pay.AddRequestData("vnp_TmnCode", _configuration["Vnpay:TmnCode"]);
-            pay.AddRequestData("vnp_Amount", ((int)model.Amount * 100).ToString());
+            pay.AddRequestData("vnp_Amount", ((long)model.Amount * 100).ToString());
             pay.AddRequestData("vnp_CreateDate", timeNow.ToString("yyyyMMddHHmmss"));
             pay.AddRequestData("vnp_CurrCode", _configuration["Vnpay:CurrCode"]);
             pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(context));
